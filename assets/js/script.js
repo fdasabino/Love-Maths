44a5eb408b3d2ff886a8f2f01885dab0b526d100
Francisco Sabino <fdasabino@gmail.com>
2021-05-10 22:59:20 +0200

    added code to make compare results and display right ones of wrong

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	for (let button of buttons) {
 		button.addEventListener("click", function () {
 			if (this.getAttribute("data-type") === "submit") {
-				alert("You clicked submit!");
+				checkAnswer();
 			} else {
 				let gameType = this.getAttribute("data-type");
 				runGame(gameType);
@@ -29,10 +29,42 @@ function runGame(gameType) {
 		throw "Unknown game type: ${gameType}. Aborting!";
 	}
 }
+/**
+ * Checks the answer against the first element in the returned
+ * calculateCorrectAnswer array.
+ */
+function checkAnswer() {
+	let userAnswer = parseInt(document.getElementById("answer-box").value);
+	let calculatedAnswer = calculateCorrectAnswer();
+	let isCorrect = userAnswer === calculatedAnswer[0];
 
-function checkAnswer() {}
+	if (isCorrect) {
+		alert("Hey you got it right! :D");
+	} else {
+		alert(
+			//important to use backticks as quotations can be rather confusing
+			`Sorry${userAnswer} is wrong. The correct answer was ${calculatedAnswer[0]}!`
+		);
+	}
+	runGame(calculatedAnswer[1]);
+}
 
-function calculateCorrectAnswer() {}
+/**
+ * Gets the operands (the numbers) and the operator (+-*...)
+ * directly from the DOM, and returns the correct answer.
+ */
+function calculateCorrectAnswer() {
+	let operand1 = parseInt(document.getElementById("operand1").innerText);
+	let operand2 = parseInt(document.getElementById("operand2").innerText);
+	let operator = document.getElementById("operator").innerText;
+
+	if (operator === "+") {
+		return [operand1 + operand2, "addition"];
+	} else {
+		alert("Unimplemented operator ${operator}");
+		throw "Unimplemented operator ${operator}. Aborting!";
+	}
+}
 
 function incrementScore() {}
 
